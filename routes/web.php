89eb02032d3f04d4ec6a7a9966000cbceed3855a
Shaun Thornburgh <personal@shaunthornburgh.com>
2020-11-07 10:23:27 +0000

    Install and configure VueRouter

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{all}', function () {
     return view('welcome');
-});
+})->where('all', '^(?!api\/)[\/\w\.-]*');
+
