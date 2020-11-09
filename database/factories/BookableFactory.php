110675d11526b626f98fcc736db75774656c2b58
Shaun Thornburgh <personal@shaunthornburgh.com>
2020-11-09 20:00:38 +0000

    Bookable table migration and seeds

---
+++ database/factories/BookableFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bookable;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+class BookableFactory extends Factory
+{
+    private $suffix = [
+        'Villa',
+        'House',
+        'Cottage',
+        'Luxury Villas',
+        'Cheap House',
+        'Rooms',
+        'Cheap Rooms',
+        'Luxury Rooms',
+        'Fancy Rooms'
+    ];
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Bookable::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->city . ' ' . Arr::random($this->suffix),
+            'description' => $this->faker->text()
+        ];
+    }
+}
