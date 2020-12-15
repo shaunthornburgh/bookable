5969d8d44cee67829328bb9a7239714f29358c2d
Shaun Thornburgh <personal@shaunthornburgh.com>
2020-12-15 22:25:48 +0000

    Single action controller with valiadtion

---
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Bookable;
+use Illuminate\Http\Request;
+
+class BookableAvailabilityController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Bookable $bookable
+     * @return void
+     */
+    public function __invoke(Request $request)
+    {
+        $data = $request->validate([
+            'from' => 'required|date_format:Y-m-d|after_or_equal:now',
+            'to' => 'required|date_format:Y-m-d|after_or_equal:from'
+        ]);
+
+        dd($data);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\BookableAvailabilityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Bookable;
@@ -29,3 +30,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //});
 
 Route::apiResource('bookables', BookableController::class);
+Route::get('bookables/{bookable}/availability', BookableAvailabilityController::class)
+    ->name('bookables.availability.show');
