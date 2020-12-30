711481e7d9df10b50c8f268a6a3ca89e839f2df0
Shaun Thornburgh <personal@shaunthornburgh.com>
2020-12-30 10:55:18 +0000

    Use async/await in Review component

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -140,30 +140,23 @@
                 sending: false
             };
         },
-        created() {
+        async created() {
             this.review.id = this.$route.params.id;
             this.loading = true;
-            axios
-                .get(`/api/reviews/${this.review.id}`)
-                .then(response => {
-                    this.existingReview = response.data.data;
-                })
-                .catch(err => {
-                    if (is404(err)) {
-                        return axios
-                            .get(`/api/booking-by-review/${this.review.id}`)
-                            .then(response => {
-                                this.booking = response.data.data;
-                            })
-                            .catch(err => {
-                                this.error = !is404(err);
-                            });
+            try {
+                this.existingReview = (await axios.get(`/api/reviews/${this.review.id}`)).data.data;
+            } catch (err) {
+                if (is404(err)) {
+                    try {
+                        this.booking = (await axios.get(`/api/booking-by-review/${this.review.id}`)).data.data;
+                    } catch (err) {
+                        this.error = !is404(err);
                     }
+                } else {
                     this.error = true;
-                })
-                .then(() => {
-                    this.loading = false;
-                });
+                }
+            }
+            this.loading = false;
         },
         computed: {
             alreadyReviewed() {
